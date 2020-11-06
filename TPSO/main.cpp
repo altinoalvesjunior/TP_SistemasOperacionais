1bd5bc308efc90fda1497f0b7f052a9de9eb53f0
Altino Júnior <altinoalvesjunior@gmail.com>
2020-11-06 18:21:54 -0300

    ATL: escrita e leitura okay

--- TPSO/main.cpp
+++ TPSO/main.cpp
@@ -4,54 +4,26 @@
 #include <sstream>
 #include <stdio.h>
 #include <string.h>
-// #include <bits/stdc++.h>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-struct estrutura
-{
-    string sequenciaReferencia;
-    int numeroPaginas;
-};
-
 void menu();
 void escreverArquivo();
-
-void convert_array(string str){
-    int str_length = str.length();
-    int arr[str_length];
-    arr[str_length] = { 0 };
-    int j = 0, i, sum = 0;
-    //traversing the string
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] == ', ') {
-            j++;
-        }
-        else {
-            arr[j] = arr[j] * 10 + (str[i] - 48);
-        }
-    }
-    cout << "arr[] = ";
-    for (i = 0; i <= j; i++) {
-        cout << arr[i] << " ";
-        sum += arr[i];
-    }
-    cout << "\nSum of array is = " << sum << endl;
-}
 
 int main() {
 
     ifstream arquivo;
 
-    string sequenciaReferencia;
+    string linha1;
     string vazio;
     string linha3;
 
     arquivo.open("teste.txt");
 
     if(arquivo.is_open()) {
-        getline(arquivo, sequenciaReferencia);
-        //cout << sequenciaReferencia << endl;
+        getline(arquivo, linha1);
         getline(arquivo, vazio);
         getline(arquivo, linha3);
         arquivo.close();
@@ -59,36 +31,23 @@ int main() {
         cout << "Erro ao abrir o arquivo!" << endl;
     }
 
-    //Converte string em vetor char, ainda necessita ajustes
-    int tam = sequenciaReferencia.length();
-    char p[tam];
-    strcpy(p, sequenciaReferencia.c_str());
+    vector<int> sequenciaReferencia;
+    stringstream sstream(linha1);
 
-    cout << "Vetor: ";
-    for(int i = 0; i< sizeof(p); i++){
-        if(p[i] != ',' && p[i] != ' '){
-            cout << "[" << p[i] << "]";
-        }
+    for (int i; sstream >> i;) {
+        sequenciaReferencia.push_back(i);
+        if (sstream.peek() == ',' || sstream.peek() == ' ')
+            sstream.ignore();
     }
 
-    // Converte linha do arquivo de quadros em inteiro
+    cout << "O vetor eh: ";
+
+    for (int i = 0; i < sequenciaReferencia.size(); i++)
+        cout << "[" << sequenciaReferencia[i] << "]";
+
     int quadros = stoi(linha3);
     cout << endl << "A quantidade de quadros é: ";
     cout << quadros << endl;
-
-    /*dif
-
-    stringstream ss(quadros);
-    vector<int> v;
-
-    int hlp;
-    while(ss >> hlp)
-    {
-        v.push_back(hlp);
-    }
-
-    for(auto i: v)
-    cout << i << '\n'; */
 
     escreverArquivo();
 
