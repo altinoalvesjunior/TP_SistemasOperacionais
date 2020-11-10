59ade3ac11a31bc01982aee487e49835f1aea02e
Altino Júnior <altinoalvesjunior@gmail.com>
2020-11-10 20:18:53 -0300

    transformando leitura em função

--- TPSO/main.cpp
+++ TPSO/main.cpp
@@ -6,14 +6,18 @@
 
 using namespace std;
 
+int quadros;
+
+
 void menu();
+vector<int> lerArquivo(std::ifstream& arquivo);
 void escreverArquivo();
 
 int main() {
 
     ifstream arquivo;
 
-    string linha1;
+    /*string linha1;
     string linha2;
 
     arquivo.open("/Users/altino/Documents/TP_SistemasOperacionais/TPSO/teste.txt");
@@ -43,9 +47,18 @@ int main() {
 
     int quadros = stoi(linha2);
     cout << endl << "A quantidade de quadros é: ";
-    cout << quadros << endl;
+    cout << quadros << endl;*/
 
     escreverArquivo();
+
+    vector <int> sequencia;
+    sequencia = lerArquivo(arquivo);
+
+   /* for(int i = 0; i < sequencia.size(); i++) {
+        cout << "[" <<  sequencia[i] << "]";
+    }*/
+
+    FIFO(sequencia);
 
     return 0;
 }
@@ -55,6 +68,37 @@ void menu(){
     cout << endl << " 1 - Importar arquivo" << endl;
     cout << " 2 - Exportar arquivo" << endl;
     cout << endl << "Escolha: ";
+}
+
+vector<int> lerArquivo(std::ifstream& arquivo) {
+
+    string linha1;
+    string linha2;
+
+    arquivo.open("/Users/altino/Documents/TP_SistemasOperacionais/TPSO/teste.txt");
+
+    if(arquivo.is_open()) {
+        getline(arquivo, linha1);
+        getline(arquivo, linha2);
+        arquivo.close();
+    } else {
+        cout << "Erro ao abrir o arquivo!" << endl;
+        exit(1);
+    }
+
+    vector<int> vetor;
+    stringstream sstream(linha1);
+
+    for (int i; sstream >> i;) {
+        vetor.push_back(i);
+
+        if (sstream.peek() == ',' || sstream.peek() == ' ' || sstream.peek() == '.')
+            sstream.ignore();
+    }
+
+    quadros = stoi(linha2);
+
+    return vetor;
 }
 
 void escreverArquivo() {
@@ -69,3 +113,4 @@ void escreverArquivo() {
 
     arquivoSaida.close();
 }
+
